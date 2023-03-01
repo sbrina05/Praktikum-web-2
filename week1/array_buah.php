6439164788c38b2243c14bd57b82997e3a1ae77a
sbrina05 <114566573+sbrina05@users.noreply.github.com>
2023-03-01 12:43:19 +0700

    Add files via upload

---
+++ week1/array_buah.php
@@ -0,0 +1,31 @@
+<?php
+$ar_buah = ["Pepaya","Mangga","Pisang","Jambu"];
+
+echo "Ini adalah hasil data buah dalam array";
+// cetak buah dengan index ke 2
+echo "<br/>Ini buah index ke 2 - $ar_buah[2]";
+// cetak total buah
+echo '<br/>Total buah adalah ' .count($ar_buah);
+
+// cetak seluruh data array buah
+echo "<ol>";
+foreach ($ar_buah as $buah ) {
+    echo "<li>$buah</li>";
+}
+echo "</ol>";
+
+// Tambahkan buah
+$ar_buah[] = "Durian";
+
+// Hapus Buah
+unset($ar_buah[1]);
+
+//ubah buah index ke dua menjadi manggis
+$ar_buah[2] = "Manggis";
+
+echo "<ol>";
+foreach ($ar_buah as $buah) {
+    echo "<li>$buah</li>";
+}
+echo "</ol>";
+?>
---
+++ week1/test.php
@@ -0,0 +1,21 @@
+<?php
+//Membuat variable
+$nama ="Sabrina Fajriyah";
+// cara mencetak variable
+echo $nama;
+
+// =======================
+// definisikan variable
+$nama = 'Sabrina';
+$umur = 18;
+$berat = 52;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. ' Tahun ';
+echo '<br/>Berat : ' .$berat.' kg ';
+
+echo "<hr>";
+
+echo "<br/> Hello $nama Apakabar?";
+echo "<br/> Hello kabar saya baik";
+?>
