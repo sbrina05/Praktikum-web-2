236cdcd629d1dc53c39c684c155a18a8afa33dc8
sbrina05 <114566573+sbrina05@users.noreply.github.com>
2023-03-12 22:24:58 +0700

    Add files via upload

---
+++ week3/template/content.php
@@ -0,0 +1,7 @@
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4>Dashboard</h1>
+            <p> Hello World ini adalah dasboard<p>
+        </div>
+    </main>
